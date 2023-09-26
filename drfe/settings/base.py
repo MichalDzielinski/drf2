f2c6a7e4802f21f02b1e2c1795a6d5922e62ff10
Michał Dzieliński <dzielinski.michal@gmail.com>
2023-09-26 12:00:03 +0200

    A

--- drfe/settings/base.py
+++ drfe/settings/base.py
@@ -1,15 +1,15 @@
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6*i7!wzvw!(r7s+i&ms$r&c^&ay!g_a3vzxqx)hgwd860b!u%('
+load_dotenv()
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     #
     'product',
 
@@ -89,3 +90,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    
+}
