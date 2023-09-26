50e30c56ef75a8113ad34827300b1cfafbca7e61
Michał Dzieliński <dzielinski.michal@gmail.com>
2023-09-26 11:18:29 +0200

    Initial

---
+++ drfe/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for drfe project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drfe.settings')
+
+application = get_asgi_application()
